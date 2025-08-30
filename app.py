31aa3c1a19ca30c5a02c45c35ecda63cb1b24f52
🤖Sushant <sushantchoudhary912@gmail.com>
2025-08-30 17:10:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 # FastAPI backend URL
-API_URL = "http://127.0.0.1:8000"
+API_URL = "https://todo-backend.onrender.com"
 
 st.set_page_config(page_title="To-Do List", page_icon="📝", layout="centered")
 
